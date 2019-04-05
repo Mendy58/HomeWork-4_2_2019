15648b60b8b9391882a1c40f2cfae49f469ef569
Mendy <Mendelmullerpupa@gmail.com>
2019-04-04 21:44:14 -0400

    Tada

--- HomeWork 4_2_2019/Controllers/HomeController.cs
+++ HomeWork 4_2_2019/Controllers/HomeController.cs
@@ -1,30 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PictureManagerLibrary;
 
 namespace HomeWork_4_2_2019.Controllers
 {
     public class HomeController : Controller
     {
+        PicturedbManager mgr = new PicturedbManager(Properties.Settings.Default.ConStr);
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult AddPassword(string Password, int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            if(mgr.GetPasswords().Contains(Password))
+            {
+                HttpCookie FromRequest = Request.Cookies["Passwords"];
+                string pwords = "";
+                if (FromRequest != null)
+                {
+                    pwords = FromRequest.Value.ToString();
+                }
+                pwords += $",{Password}";
+                HttpCookie cookie = new HttpCookie("Passwords", pwords);
+                Response.Cookies.Add(cookie);
+            }
+            return Redirect($"~/Home/Image?id={id}");
         }
 
-        public ActionResult Contact()
+        public ActionResult Image(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            HttpCookie FromRequest = Request.Cookies["Passwords"];
+            var Passwords = new List<string>();
 
-            return View();
+            if (FromRequest != null)
+            {
+                string userids = FromRequest.Value.ToString();
+                Passwords = userids.Split(',').ToList();
+            }
+
+            Picture pic = mgr.GetPictureByid(id);
+
+            if(!Passwords.Contains(pic.Password) || Passwords == null)
+            {
+                pic.Name = null;
+            }
+            else
+            {
+                mgr.AddCountToPic(id);
+            }
+
+            return View(pic);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase image, string description, string Password)
+        {
+            string ext = Path.GetExtension(image.FileName);
+            string fileName = $"{Guid.NewGuid()}{ext}";
+            string fullPath = $"{Server.MapPath("/UploadedPictures")}\\{fileName}";
+            image.SaveAs(fullPath);
+            
+            int x = mgr.AddPic(new Picture
+            {
+                Name = fileName,
+                Description = description,
+                Password = Password
+            });
+            return (RedirectToAction("Link", new { message = $"/Home/Image?id=" + x }));
+        }
+        public ActionResult Link(string message)
+        {
+            return View(message);
         }
     }
 }
---
+++ PictureManagerLibrary/PicturedbManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureManagerLibrary
+{
+    public class PicturedbManager
+    {
+        private string _connectionstring;
+        public PicturedbManager(string _ConnectionString)
+        {
+            _connectionstring = _ConnectionString;
+        }
+
+        public List<string> GetPasswords()
+        {
+            var p = new List<string>();
+            using (var Con = new SqlConnection(_connectionstring))
+            using (var cmd = Con.CreateCommand())
+            {
+                cmd.CommandText = @"Select Password from Images";
+                Con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    p.Add((string)reader["Password"]);
+                }
+            }
+            return p;
+        }
+
+        public Picture GetPictureByid(int id)
+        {
+            Picture p = new Picture();
+            using (var Con = new SqlConnection(_connectionstring))
+            using (var cmd = Con.CreateCommand())
+            {
+                cmd.CommandText = @"Select * from Images
+                                  Where id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                Con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.Read())
+                {
+                    return null;
+                }
+                p.id = (int)reader["id"];
+                p.Name = (string)reader["Name"];
+                p.Description = (string)reader["Description"];
+                p.Password = (string)reader["Password"];
+                p.Count = (int)reader["Count"];
+            }
+            return p;
+        }
+        public int AddPic(Picture picture)
+        {
+            int x;
+            using (var con = new SqlConnection(_connectionstring))
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = @"INSERT INTO Images
+                                     VALUES (@Name, @Description,0,@Password)
+                                             Select Scope_Identity();";
+                cmd.Parameters.AddWithValue("@Name", picture.Name);
+                cmd.Parameters.AddWithValue("@Description", picture.Description);
+                cmd.Parameters.AddWithValue("@Password", picture.Password);
+                con.Open();
+                x = (int) (decimal) cmd.ExecuteScalar();
+            }
+            return x;
+        }
+        public void AddCountToPic(int id)
+        {
+            using (var con = new SqlConnection(_connectionstring))
+            using (var cmd = con.CreateCommand())
+            {
+                cmd.CommandText = @"UPDATE Images  SET Count = Count + 1 WHERE id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+    //Select Password from Images
+    //UPDATE Images  SET Count = Count + 1 WHERE id = 1
+}
